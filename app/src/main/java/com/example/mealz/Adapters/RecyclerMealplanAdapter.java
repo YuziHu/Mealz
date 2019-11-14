9d824da86345927086e4abac63615dd7074d8c59
yuzihu <yhu277@wisc.edu>
2019-11-14 16:08:30 -0500

    implement two layouts on the same mealplan adapter

--- app/src/main/java/com/example/mealz/Adapters/RecyclerMealplanAdapter.java
+++ app/src/main/java/com/example/mealz/Adapters/RecyclerMealplanAdapter.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,9 +19,11 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerMealplanAdapter extends RecyclerView.Adapter<RecyclerMealplanAdapter.ViewHolder> {
+public class RecyclerMealplanAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private static final String TAG = "RecyclerMealplanAdapter";
+
+//    private static final int PENDING_MEALPLAN_LISTVIEW = 1;
 
     private List<String> imageUrls;
     private List<String> mealplanNames;
@@ -39,21 +42,37 @@ public class RecyclerMealplanAdapter extends RecyclerView.Adapter<RecyclerMealpl
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Log.d(TAG, "onCreateViewHolder: called");
-
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_single_mealplan, parent, false);
-        return new ViewHolder(view, onMealplanClickListener);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: called" + viewType);
+        View view = null;
+        if(tag=="PENDING"){
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_single_mealplan_pending, parent, false);
+            return new ViewHolderPending(view, onMealplanClickListener);
+        }
+        else{
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_single_mealplan, parent, false);
+            return new ViewHolderOther(view, onMealplanClickListener);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof ViewHolderPending) {
+            if(imageUrls!=null && imageUrls.size()>0){
+                Glide.with(context)
+                        .asBitmap()
+                        .load(imageUrls.get(position))
+                        .into(((ViewHolderPending) holder).image);
+                ((ViewHolderPending) holder).name.setText(mealplanNames.get(position));
+            }
+        }
+        else
         if(imageUrls!=null && imageUrls.size()>0){
             Glide.with(context)
                     .asBitmap()
                     .load(imageUrls.get(position))
-                    .into(holder.image);
-            holder.name.setText(mealplanNames.get(position));
+                    .into(((ViewHolderOther) holder).image);
+            ((ViewHolderOther) holder).name.setText(mealplanNames.get(position));
 
         }
     }
@@ -63,12 +82,44 @@ public class RecyclerMealplanAdapter extends RecyclerView.Adapter<RecyclerMealpl
         return mealplanNames.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    @Override
+    public int getItemViewType(int position) {
+        if(tag=="PENDING") return 1;
+        else return 0;
+    }
+
+    public class ViewHolderPending extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView image;
+        TextView name;
+        // for pending mealplan list
+        Button dislike;
+        Button like;
+        MealPlanClickListener onMealplanClickListener;
+
+        public ViewHolderPending(@NonNull View itemView, MealPlanClickListener onMealplanClickListener) {
+            super(itemView);
+            image = itemView.findViewById(R.id.mealplan_image);
+            name = itemView.findViewById(R.id.mealplan_name);
+            dislike = itemView.findViewById(R.id.dislike_button);
+            like = itemView.findViewById(R.id.like_button);
+            this.onMealplanClickListener = onMealplanClickListener;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "onClick: "+tag);
+            onMealplanClickListener.onMealplanClick(tag, getAdapterPosition());
+        }
+    }
+
+    public class ViewHolderOther extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView image;
         TextView name;
         MealPlanClickListener onMealplanClickListener;
 
-        public ViewHolder(@NonNull View itemView, MealPlanClickListener onMealplanClickListener) {
+        public ViewHolderOther(@NonNull View itemView, MealPlanClickListener onMealplanClickListener) {
             super(itemView);
             image = itemView.findViewById(R.id.mealplan_image);
             name = itemView.findViewById(R.id.mealplan_name);
